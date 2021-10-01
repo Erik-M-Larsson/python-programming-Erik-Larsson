5959af1d0a0833aa2ffb9feb9eb41bf9cce4c475
Erik-M-Larsson <89965231+Erik-M-Larsson@users.noreply.github.com>
2021-10-01 17:10:21 +0200

    Klass Cirkel

--- Labs/Lab3/köör.py
+++ Labs/Lab3/köör.py
@@ -1,8 +1,29 @@
 import lab3 as l
 
-form = l.Geometrisk_form(5, 4)
-form.rita_ut()
+#form = l.Geometrisk_form(5, 4)
+#form.rita_ut()
 
-form.flööö(3, 3 )
+#form.flööö(3, 3 )
 
-form.flööö(0, -6, True )
+#form.flööö(0, -6, True )
+
+
+#cirkel1 = l.Cirkel(5, 4, 4)
+#cirkel2 = l.Cirkel(5, 4, 3.2)
+cirkel3 = l.Cirkel(5, 4, 1)
+
+#cirkel.flööö(3, 0)
+#cirkel1.flööö(3, 3, True)
+
+#print(cirkel1 == cirkel2)
+#print("\U0001F49A",'\u03C4')
+
+#print(cirkel1.inne_i(1, 1))
+#print(cirkel1.inne_i(5, 0))
+#print(cirkel1.inne_i(5, 1))
+
+#print(cirkel3.area())
+#print(cirkel3.omkrets())
+
+#print(cirkel3)
+
--- Labs/Lab3/lab3.py
+++ Labs/Lab3/lab3.py
@@ -4,12 +4,16 @@ import numpy as np
 from math import tau
 
 
+class XYplan:
+    pass
+
 class Geometrisk_form:
     """Klass för geometriska former"""
 
     def __init__(self, x: float, y: float) -> None:
         self.x = x # TODO Tillkalla flööö istället?
         self.y = y
+        self.rita_ut()
 
 
     @property
@@ -54,18 +58,58 @@ class Geometrisk_form:
         fig, ax = plt.figure(),plt.axes() 
 
         ax.plot(self.x, self.y, "ro")
-        # TODO plot randen på formen
+        ax.plot(self.x + self._xx, self.y + self._yy, "r-")
 
-        ax.set(title="Nått smart", xlabel="x", ylabel="y")
+        ax.set(title="Nått smart", xlabel="x", ylabel="y", aspect='equal')
         plt.show()
 
+    def __repr__(self) -> str: # TODO skriv nått i denna
+        pass
 
 
 class Rektangel(Geometrisk_form):
     pass
 
 class Cirkel(Geometrisk_form):
-    pass
+    "Klass för cirklar"
+
+    def __init__(self, x: float, y: float, r :float) -> None:
+        self.r = r        
+        super().__init__(x, y)
+        
+    @property
+    def r(self) -> float:
+        return self._r
+
+    @r.setter
+    def r(self, val: float) -> None:
+        self.test_reelt(val)
+        self._r = val
+
+    def omkrets(self) -> float:
+        print("\U0001F49A", '\u03C4')
+        return tau * self.r     
+    
+    def area(self) -> float:
+        return tau * self.r**2 /2       
+
+    def inne_i(self, x: float, y: float) -> bool:
+        avstånd_mitt = np.sqrt((self.x - x)**2 + (self.y - y)**2)
+        return avstånd_mitt <= self.r
+
+    def __eq__(self, other: "Cirkel") -> bool:
+        return self.r == other.r
+
+    def rita_ut(self) -> None:
+
+        self._xx = self.r * np.cos(np.linspace(0, tau)) # TODO Hitta bättre variabelnamn än  xx
+        self._yy = self.r * np.sin(np.linspace(0, tau)) 
+
+        return super().rita_ut()
+        
+    def __repr__(self) -> str:
+        return f"Cirkel( x = {self.x}, y = {self.y}, r = {self.r})"
+
 
 class Kub:
     pass
